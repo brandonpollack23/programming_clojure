fb52eb630af1e02958398eb6a49ed8df36b366f1
Brandon Pollack <brandonpollack23@gmail.com>
2021-05-13 22:06:36 -0400

    Changed to atom

--- snake/src/snake/core.clj
+++ snake/src/snake/core.clj
@@ -1,5 +1,5 @@
 (ns snake.core
-  (:import (java.awt Color Dimension)
+  (:import (java.awt Color Graphics Dimension)
            (javax.swing JPanel JFrame Timer JOptionPane)
            (java.awt.event ActionListener KeyListener))
   (:require [org.baznex.imports :refer [import-static]]
@@ -17,10 +17,13 @@
            VK_UP    [0 -1]
            VK_DOWN  [0 1]})
 
+;; Specs
 (s/def ::coordinate (s/tuple int? int?))
 (s/def ::rect (s/cat :x int? :y int? :l int? :w int?))
 (s/def ::canvas-object (s/keys :req-un [::type]))
 (s/def ::direction (s/and (s/tuple int? int?) #(#{[0 1] [0 -1] [1 0] [-1 0]} %)))
+
+;; Functional Model
 
 (s/fdef add-points
   :args (s/* ::coordinate)
@@ -91,7 +94,78 @@
   [{body :body}]
   (>= (count body) win-length))
 
+;; Begin Mutable Model
+
+(defn reset-game [_]
+  {:snake (create-snake)
+   :apple (create-apple)})
+
+(defn update-direction [{:keys [snake] :as game-state} newdir]
+  (if newdir
+    (assoc game-state :snake (turn snake newdir))
+    game-state))
+
+(defn update-positions [{:keys [snake apple] :as game-state}]
+  (if (eats? snake apple)
+    (assoc game-state :apple (create-apple) :snake (move snake :grow))
+    (assoc game-state :snake (move snake))))
+
+;; Swing GUI
+;;
+(defn fill-point [^:Graphics g ^:Color color pt]
+  (let [[x y width height] (point-to-screen-rect pt)]
+    (.setColor g color)
+    (.fillRect g x y width height)))
+
+(defmulti paint (fn [^:Graphics g object & _] (:type object)))
+(defmethod paint :apple [^:Graphics g {:keys [location color]}]
+  (fill-point g color location))
+(defmethod paint :snake [^:Graphics g {:keys [body color]}]
+  (doseq [point body]
+    (fill-point g color point)))
+
+(defn game-panel [^:JFrame frame game-state]
+  (proxy [JPanel ActionListener KeyListener] []
+    (paintComponent [^:Graphics g]
+      (proxy-super paintComponent g)
+      (paint g (@game-state :snake))
+      (paint g (@game-state :apple)))
+    (actionPerformed [e]
+      (swap! game-state update-positions)
+      (when (head-overlaps-body? (@game-state :snake))
+        (swap! game-state reset-game)
+        (JOptionPane/showMessageDialog frame "You lose!"))
+      (when (win? (@game-state :snake))
+        (swap! game-state reset-game)
+        (JOptionPane/showMessageDialog frame "You win!"))
+      (.repaint this))
+    (keyPressed [e]
+      (swap! game-state update-direction (dirs (.getKeyCode e))))
+    (getPreferredSize []
+      (Dimension. (* (inc width) point-size)
+                  (* (inc height) point-size)))
+    (keyReleased [e])
+    (keyTyped [e])))
+
+(defn game
+  "Creates a new game in the swing UI Runtime"
+  []
+  (let [game-state (atom (reset-game {}))
+        frame (JFrame. "Snake")
+        panel (game-panel frame game-state)
+        timer (Timer. turn-millis panel)]
+    (doto panel
+      (.setFocusable true)
+      (.addKeyListener panel))
+    (doto frame
+      (.add panel)
+      (.pack)
+      (.setVisible true))
+    (.start timer)
+    [game-state, timer]))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Launching snake game in swing ui...")
+  (game))
